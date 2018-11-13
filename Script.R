304d026ef5e60b033c90799d427b3b47ef84a79f
1AdamG <a.ghneim@gmail.com>
2018-11-13 10:31:59 +0100

    Added comments

--- Script.R
+++ Script.R
@@ -1,5 +1,6 @@
 library('dbConnect')
 
+#Change mysql username and password below
 ImportMangrooveMySQL <- function(mysql.server.name = "127.0.0.1", mysql.server.port = 3307, mysql.database = "NTDB_adam", mysql.username = "x", mysql.password = "x", mysql.Mangroove.table = "2012_summary") {
     mydb <- dbConnect(MySQL(), user = mysql.username, password = mysql.password, dbname = mysql.database, host = mysql.server.name, port = mysql.server.port)
     ## Select all data from database
