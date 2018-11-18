4a57dfdb270447441b8722fe3da03a7d0d230d66
1AdamG <a.ghneim@gmail.com>
2018-11-18 09:28:31 +0100

    Added correct confidence loop

--- R/ImportMySQL.R
+++
@@ -1,7 +0,0 @@
-ImportMangrooveMySQL <- function(mysql.server.name, mysql.server.port, mysql.database, mysql.username, mysql.password, mysql.Mangroove.table = "2012_summary") {
-    mydb <- dbConnect(MySQL(), user = mysql.username, password = mysql.password, dbname = mysql.database, host = mysql.server.name, port = mysql.server.port)
-    ## Select all data from database
-    study.data <- dbGetQuery(mydb, sprintf("select * from %s", mysql.Mangroove.table))
-    dbDisconnect(mydb) ## Disconnect from database
-    return(study.data)
-}
--- Script.R
+++ Script.R
@@ -10,10 +10,12 @@ library('dbConnect')
 ## The number of events in datasetA and C should always be 200, and the number
 ## of non-events should vary depending on the outcome prevalence
 numberofevents <- 200
+confidenceinterval = c(0.02, 0.05, 0.10)
+
 
 #Initialize
 source(".sshconfig.R")
-source("R/ImportMySQL.R")
+source("R/MySQLFunctions.R")
 source("R/CreateSubSample.R")
 
 #Get Datasets
@@ -21,16 +23,20 @@ print("Get Datasets")
 datasetA <- ImportMangrooveMySQL(mysql.server.name, mysql.server.port, mysql.database, mysql.username, mysql.password, mysql.Mangroove.table = '2012_summary')
 datasetB <- ImportMangrooveMySQL(mysql.server.name, mysql.server.port, mysql.database, mysql.username, mysql.password, mysql.Mangroove.table = '2013_summary')
 datasetC <- ImportMangrooveMySQL(mysql.server.name, mysql.server.port, mysql.database, mysql.username, mysql.password, mysql.Mangroove.table = '2014_summary')
+loopCount <- 0
 
+executionID <- format(Sys.time(), "%Y%m%d%H%M%OS")
 #Start loop number of updating events (1-1000) changes with each loop
-for (numberofupdatingevents in c(200)) { #Test with only 200
+for (numberofupdatingevents in c(1:1000)) { #Test with only 200
     print(paste("Start loop numberofupdatingevents", numberofupdatingevents))
     #Start loop confindence (0.02, 0.05, 0.10) changes with each loop. Note that the
     #outcome prevelance should always be the same in datasetB and datasetC
-    for (confindence in c(0.02)) {
-        developmentprevalence <- confindence
-        updatingvalidationprevalence <- confindence
-        print(paste("Start loop confindence", confindence))
+    
+    for (confindence in data.frame(t(expand.grid(confidenceinterval, confidenceinterval)))) {
+        loopCount <- loopCount+1
+        developmentprevalence <- confindence[1]
+        updatingvalidationprevalence <- confindence[2]
+        print(paste("Start loop confindence", developmentprevalence, updatingvalidationprevalence))
 
         ## Now define the number of non-events
         numberofdevelopmentnonevents <- ceiling((numberofevents / developmentprevalence) - numberofevents)
@@ -58,8 +64,8 @@ for (numberofupdatingevents in c(200)) { #Test with only 200
         confmatrixA <- table(Actual_value = sample.dataset.A$Event, Predicted_value = resA > 0.5)
         confmatrixA
 
-        modelMres <- (confmatrixA[[1, 1]] + confmatrixA[[2, 2]]) / sum(confmatrixA)
-        modelMres
+        #modelMres <- (confmatrixA[[1, 1]] + confmatrixA[[2, 2]]) / sum(confmatrixA)
+        #modelMres
 
 
         #Get updating data (Dataset B), pick samples
@@ -78,9 +84,14 @@ for (numberofupdatingevents in c(200)) { #Test with only 200
         confmatrixB <- table(Actual_value = sample.dataset.B$Event, Predicted_value = resB > 0.5)
         confmatrixB
 
-        modelUMres <- (confmatrixB[[1, 1]] + confmatrixB[[2, 2]]) / sum(confmatrixB)
+        #modelUMres <- (confmatrixB[[1, 1]] + confmatrixB[[2, 2]]) / sum(confmatrixB)
 
         #compare results from both models
-        modelMres - modelUMres
+        #modelMres - modelUMres
+        print("Finished")
+        print(loopCount)
+        test <- sprintf("INSERT INTO `NTDB_adam`.`runtime_data` (`executionID`,`loopCount`,`developmentprevalence`,`updatingvalidationprevalence`,`numberofdevelopmentnonevents`,`numberofvalidationnonevents`,`numberofupdatingnonevents`,`modelMIntercept`,`modelMSBP`,`modelMPulse`,`mdelMRR`,`modelMGCSTOT`,`modelUMIntercept`,`comparisonResult`) VALUES (%s,%g,%g,%g,%g,%g,%g,%g,%g,%g,%g,%g,%g,%g);", executionID, loopCount, developmentprevalence, updatingvalidationprevalence, numberofdevelopmentnonevents, numberofvalidationnonevents, numberofupdatingnonevents, 0, 0, 0, 0, 0, 0, 0)
+        print(test)
+        StoreLoopData(executionID, loopCount, developmentprevalence, updatingvalidationprevalence, numberofdevelopmentnonevents, numberofvalidationnonevents, numberofupdatingnonevents, 0, 0, 0, 0, 0, 0, 0)
     }
 }
